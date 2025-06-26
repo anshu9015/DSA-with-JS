d78ac4b3f844ded8832842c469227ecd9cb8bdb1
Anshu Kumar <anshu.kumar@velocis.in>
2025-06-26 17:47:37 +0530

    missing nmber from array

--- coding/Array/missingNumber.js
+++ coding/Array/missingNumber.js
@@ -16,3 +16,8 @@ function missingNumber(arr,n){
 }
 
 console.log(missingNumber([2,1,4],8));
+
+//first of all we take the n , which shows that till that number which is already given n should be in the array from 1.
+//then we will iterate from 1 to n and also iterate the array which matches with iteration of 1 to n which does not found should be in the another array
+//the approach is we have to take the boolean value which initially taken as false...and when we iterate with array and which found then we change the boolean value to true 
+// and in the loop from 1 to n which is false should be in the another array this is the resulted array
