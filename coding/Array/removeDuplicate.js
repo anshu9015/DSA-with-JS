0953c4b74c1d9247e358f1c7183862710fed9309
Anshu Kumar <anshu.kumar@velocis.in>
2025-06-27 18:03:19 +0530

    remove duplicate and text alignment in centre

--- coding/Array/removeDuplicate.js
+++ coding/Array/removeDuplicate.js
@@ -1,18 +1,33 @@
-function removeDuplicate(arr){
+function removeDuplicate(arr) {
     let arr1 = [];
-    for(let i = 0;i<arr.length;++i){
+    for (let i = 0; i < arr.length; ++i) {
         let found = false;
-        for(let j = i+1;j<arr.length;++j){
-            if(arr[i] === arr[j]){
+        for (let j = i + 1; j < arr.length; ++j) {
+            if (arr[i] === arr[j]) {
                 found = true;
                 break;
             }
         }
-        if(!found){
+        if (!found) {
             arr1.push(arr[i]);
         }
     }
     return arr1;
 }
 
-console.log(removeDuplicate([2,3,4,2,5]));
+console.log(removeDuplicate([2, 3, 4, 2, 5]));
+
+// function removeDuplicate2(arr) {
+//     let j = 1;
+//     for (let i = 0; i < arr.length - 1; ++i) {
+//         if (arr[i] === arr[j]) {
+//             arr.pop();
+//             j++;
+//         }
+//     }
+
+
+//     return arr;
+// }
+
+// console.log(removeDuplicate2([1,2,2,3,4]));
