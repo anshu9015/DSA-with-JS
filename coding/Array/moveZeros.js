dab688cd9a283a1941fb665adf2af4d6e1bf7f59
anshu9015 <anshu.kmr9015@gmail.com>
2025-06-29 15:43:06 +0530

    move all zeros to end by two pointer approach

--- coding/Array/moveZeros.js
+++ coding/Array/moveZeros.js
@@ -16,3 +16,18 @@ function moveZeros(arr){
     return arr1;
 }
 console.log(moveZeros([2,3,0,8,0,4]));
+
+
+function moveZeros1(arr){
+    let index = 0;
+    for(let i = 0;i<arr.length;++i){
+        if(arr[i] !== 0){
+            let temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+            index++;
+        }
+    }
+    return arr;
+}
+console.log(moveZeros1([2,3,0,8,6,3,0,0,4]));
