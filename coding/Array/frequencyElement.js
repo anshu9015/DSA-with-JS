ece1bcffa828b09ac7900186c2ae847173e24e99
Anshu Kumar <anshu.kumar@velocis.in>
2025-06-30 11:54:11 +0530

    count occurences frequency element rotate array

---
+++ coding/Array/frequencyElement.js
@@ -0,0 +1,8 @@
+function frequencyElement(arr){
+    let obj = {};
+    for(let i = 0;i<arr.length;++i){
+        obj[arr[i]] = (obj[arr[i]] || 0)+1;
+    }
+    return obj;
+}
+console.log(frequencyElement([1,2,3,4,2,1,5]));
