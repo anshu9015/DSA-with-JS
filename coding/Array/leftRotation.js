29d1680b03e3a88e79667623e87f9046459398c6
Anshu Kumar <anshu.kumar@velocis.in>
2025-06-30 11:00:56 +0530

    leftrotation by one

---
+++ coding/Array/leftRotation.js
@@ -0,0 +1,13 @@
+function leftRotation(arr) {
+
+    let j = 1;
+    for (let i = 0; i < arr.length - 1; ++i) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        j++;
+    }
+    
+    return arr;
+}
+console.log(leftRotation([1, 2, 3, 4, 5]));
