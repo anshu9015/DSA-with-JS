6c46b24d776835df86b333164d4bd82d5e7c3844
Anshu Kumar <anshu.kumar@velocis.co.in>
2025-07-03 14:09:33 +0530

    max length subarray with sum 0

--- coding/Array/subArray.js
+++ coding/Array/subArray.js
@@ -24,3 +24,27 @@ console.log(subArray([ 1, 2, -3,2,-2]))
 
 // [1,2,3]
 // [[1],[1,2],[1,2,3],[2],[2,3],[3]]
+
+
+function subArray1(arr) {
+    let res = [];
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = i; j < arr.length; ++j) {
+            res.push(arr.slice(i, j + 1));
+        }
+    }
+  let response = res.filter(item=>item.reduce((acc,curr) => acc+curr,0) === 0);
+  console.log(response);
+  let maxLength = -1;
+  let max = -1;
+  for(let i = 0;i<response.length;++i){
+    if(response[i].length>maxLength){
+        maxLength = response[i].length;
+        max = i;
+        
+    }
+  }
+  return response[max];
+}
+
+console.log(subArray1([ 1, 2, -3,2,-2]))
