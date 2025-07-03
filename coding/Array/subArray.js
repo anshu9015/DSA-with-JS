2f152ef96c2c7a04be165c6538b036a0f0203177
Anshu Kumar <anshu.kumar@velocis.co.in>
2025-07-03 12:23:44 +0530

    subarray with sum 0

--- coding/Array/subArray.js
+++ coding/Array/subArray.js
@@ -1,11 +1,26 @@
-function subArray(arr){
+function subArray(arr) {
     let res = [];
-    for(let i = 0;i<arr.length;++i){
-        for(let j = i;j<arr.length;++j){
-            res.push(arr.slice(i,j+1));
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = i; j < arr.length; ++j) {
+            res.push(arr.slice(i, j + 1));
         }
     }
-    return res.filter((item) => item.length === 2);
+    // return res;
+    
+    for (let i = 0; i < res.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < res[i].length; j++) {
+            sum = sum + res[i][j];
+
+        }
+        if (sum == 0) {
+            return res[i];
+        }
+    }
+    return -1;
 }
 
-console.log(subArray([1,2,3]))
+console.log(subArray([ 1, 2, -3,2,-2]))
+
+// [1,2,3]
+// [[1],[1,2],[1,2,3],[2],[2,3],[3]]
