6f95c6663992cb559a9afcaa1eb7440dbc938238
anshu9015 <anshu.kmr9015@gmail.com>
2025-07-19 00:07:45 +0530

    zerotwo

--- coding/Array/zeroTwoOne.js
+++ coding/Array/zeroTwoOne.js
@@ -1,17 +1,14 @@
-function sortingArray(arr) {
+function zeroTwo(arr) {
   for (let i = 0; i < arr.length - 1; ++i) {
     for (let j = i + 1; j < arr.length; ++j) {
       if (arr[i] > arr[j]) {
         let temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-        j++;
       }
     }
   }
   return arr;
 }
-
 const arr = [2, 0, 1, 2, 1, 0];
-console.log(sortingArray(arr));
-console.log(sortingArray([1, 2, 6, 4, 3, 8])); //output: [1,2,3,4,6,8]
+console.log(zeroTwo(arr));
