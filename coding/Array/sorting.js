49a53058653e37af96d392a9c616f8aae6a93d8d
Anshu Kumar <anshu.kumar@velocis.co.in>
2025-07-23 18:10:18 +0530

    sorting square pattern

--- coding/Array/sorting.js
+++ coding/Array/sorting.js
@@ -2,10 +2,7 @@ function sortingArray(arr) {
   for (let i = 0; i < arr.length - 1; ++i) {
     for (let j = i + 1; j < arr.length; ++j) {
       if (arr[i] > arr[j]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        j++;
+        [arr[i],arr[j]] = [arr[j],arr[i]];
       }
     }
   }
@@ -15,3 +12,7 @@ function sortingArray(arr) {
 const arr = [2, 0, 1, 2, 1, 0];
 console.log(sortingArray(arr));
 console.log(sortingArray([1, 2, 6, 4, 3, 8])); //output: [1,2,3,4,6,8]
+
+
+
+//bubble sort time complexity is always 0(n^2) in worst case . if the array is sorted the time complexity will go to 0(n).
