78511d9b02eaacae5f59af3dece1b76a23da5533
anshu9015 <anshu.kmr9015@gmail.com>
2025-08-08 00:09:14 +0530

    react todo app adding functionality and filtering functionality done

--- React/todo/src/App.js
+++ React/todo/src/App.js
@@ -5,98 +5,98 @@ function App() {
   const [input, setInput] = useState("");
   const [status, setStatus] = useState("");
   const [todo, setTodo] = useState([]);
-  const[filtered,setFiltered] = useState([]);
+  const [filter, setFilter] = useState("ALL");
+  const PENDING = "PENDING";
   function handleClick() {
-    if (!input.trim() || !status) {
-      alert(`please add todo and status`);
+    if (!input.trim() && !status) {
+      alert(`Please enter both todo work and status`);
       return;
     }
     const newTodo = {
       id: Date.now(),
-      input: input,
+      work: input,
       status: status,
     };
-    console.log("newTod", newTodo)
     setTodo([...todo, newTodo]);
-    setFiltered([...todo, newTodo])
-    setStatus("");
     setInput("");
+    setStatus("");
   }
-  function handleFilter(){
-    setFiltered(todo);
+  function handleFilter(e) {
+    setFilter(e.target.value);
   }
-  function handleFilter1() {
-    setFiltered(todo.filter((el) => el.status === "COMPLETED"));
+  function filteredStatus() {
+    if (filter === "COMPLETED") {
+      return todo.filter((el) => el.status === "COMPLETED");
+    } else if (filter === "PENDING") {
+      return todo.filter((el) => el.status === "PENDING");
+    } else {
+      return todo;
+    }
   }
-  function handleFilter2() {
-    setFiltered(todo.filter((el) => el.status === "PENDING"));
-  }
-  console.log("filter", filtered)
+  console.log("filtered data---->", filter);
+  const filteredArray = filteredStatus();
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      <h1>TODO LIST</h1>
       <input
         type="radio"
-        id="checkbox1"
-        name="todoActions"
-        title="todo actions"
+        id="radio1"
+        name="filter"
+        title="radio button"
+        value="ALL"
         onChange={handleFilter}
       />
-      <label htmlFor="checkbox1">ALL</label>
+      <label htmlFor="radio1">ALL</label>
       <input
         type="radio"
-        id="checkbox2"
-        name="todoActions"
-        title="todo actions"
-        onChange={handleFilter1}
+        id="radio2"
+        name="filter"
+        title="radio button"
+        value="COMPLETED"
+        onChange={handleFilter}
       />
-      <label htmlFor="checkbox2">COMPLETED</label>
+      <label htmlFor="radio2">COMPLETED</label>
       <input
         type="radio"
-        id="checkbox3"
-        name="todoActions"
-        title="todo actions"
-        onChange={handleFilter2}
+        id="radio3"
+        name="filter"
+        title="radio button"
+        value={PENDING}
+        onChange={handleFilter}
       />
-      <label htmlFor="checkbox3">PENDING</label>
-      <label htmlFor="todo">Enter Your Todo</label>
+      <label htmlFor="radio3">PENDING</label>
+      <br />
+      <br />
+      <label htmlFor="todo">Enter your TODO: </label>
       <input
         type="text"
         id="todo"
         name="todo"
-        title="enter your todo"
+        title="Enter your todo"
         value={input}
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
+        onChange={(e) => setInput(e.target.value)}
       />
       <select
-        id="select1"
-        name="select1"
-        title="select your task is pending or completed"
+        title="select your status"
         value={status}
-        onChange={(e) => setStatus(e.target.value)}
-      >
+        onChange={(e) => setStatus(e.target.value)}>
         <option value="" disabled>
-          Enter your type of task
+          Enter your status:
         </option>
-        <option value="COMPLETED">COMPLETED</option>
-        <option value="PENDING">PENDING</option>
+        <option value="COMPLETED">COMPLETED </option>
+        <option value="PENDING">PENDING </option>
       </select>
       <button type="submit" title="submit button" onClick={handleClick}>
         ADD TODO
       </button>
       <ul>
-      
-        {filtered.length &&
-          filtered.map((todos) => {
-            return (
+        {filteredArray.length
+          ? filteredArray.map((todos) => (
               <li key={todos.id}>
-                {todos.input} ---- {todos.status}
+                {todos.work} ---- {todos.status}
               </li>
-            );
-          })}
-      
+            ))
+          : null}
       </ul>
     </div>
   );
