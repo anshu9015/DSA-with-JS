0ea4a29667a7fc62428eaadf9e9d736ae11c8884
anshu9015 <anshu.kmr9015@gmail.com>
2025-08-08 01:28:31 +0530

    recursion question striver done

--- Recursion/ques1.js
+++ Recursion/ques1.js
@@ -16,35 +16,76 @@ function printNameN(n) {
 }
 console.log(printNameN(5));
 
-
-function oneToN(n){
-    if(n === 1){
-        return "1";
-    }
-    return oneToN(n-1) + "\n" + n;
+function oneToN(n) {
+  if (n === 1) {
+    return "1";
+  }
+  return oneToN(n - 1) + "\n" + n;
 }
 
-console.log(oneToN(5))
+console.log(oneToN(5));
 
-
-function nToOne(n){
-    if(n===1){
-        return n;
-    }
-    return n+"\n"+nToOne(n-1);
+function nToOne(n) {
+  if (n === 1) {
+    return n;
+  }
+  return n + "\n" + nToOne(n - 1);
 }
 
-console.log(nToOne(5))
+console.log(nToOne(5));
 
-function sumNnumber(n){
-    let sum = 0;
-    if(n===1){
-        sum += 1;
-        return sum;
-    }
-    return sumNnumber(n-1)+n;
+function sumNnumber(n) {
+  let sum = 0;
+  if (n === 1) {
+    sum += 1;
+    return sum;
+  }
+  return sumNnumber(n - 1) + n;
 }
-console.log(sumNnumber(5))
+console.log(sumNnumber(5));
+
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(5));
+
+function reverseArray(arr, start, end) {
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    reverseArray(arr, start + 1, end - 1);
+    return arr;
+  }
+}
+console.log(reverseArray([1, 2, 3, 4, 5], 0, 4));
+
+function pallindromeString(abc) {
+  let str = abc.split("").reverse().join("");
+  if (str === abc) {
+    return true;
+  }
+  return false;
+}
+console.log(pallindromeString("aba"));
+
+function recursivePallindrome(i, abc) {
+  if (i >= abc.length / 2) return true;
+  if (abc[i] !== abc[abc.length - i - 1]) return false;
+  return recursivePallindrome(i + 1, abc);
+}
+
+console.log(recursivePallindrome(0, "abc"));
+
+function fibonacci(n, a = 0, b = 1, result = "") {
+  if (n === 0) {
+    return result.trim();
+  }
+  result += a + " ";
+  return fibonacci(n - 1, b, a + b, result);
+}
+console.log(fibonacci(5));
 //learn what is recursion
 //types of recursion
 //recursion stack
