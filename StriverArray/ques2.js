ecc31fcbced1327bcf0032d5a93cc5f9887a64f2
anshu9015 <anshu.kmr9015@gmail.com>
2025-08-13 21:30:38 +0530

    majority element

--- StriverArray/ques2.js
+++ StriverArray/ques2.js
@@ -91,3 +91,60 @@ function sort102(arr) {
 }
 
 console.log(sort102([2, 0, 2, 1, 1, 0]));
+
+function majorityElement(arr) {
+  let newMap = new Map();
+  for (let i = 0; i < arr.length; ++i) {
+    newMap.set(arr[i], (newMap.get(arr[i]) || 0) + 1);
+    if (newMap.get(arr[i]) > arr.length / 2) {
+      return arr[i];
+    }
+  }
+  return -1;
+}
+
+console.log(majorityElement([4, 4, 2, 4, 3, 4, 4, 3, 2, 4]));
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
+
+function majorityElement1(arr) {
+  let count = 0;
+  let element;
+  for (let i = 0; i < arr.length; ++i) {
+    if (count === 0) {
+      count = 1;
+      element = arr[i];
+    } else if (arr[i] === element) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+  let count1 = 0;
+  for (let val of arr) {
+    if (val === element) {
+      count1++;
+    }
+  }
+  if (count1 > Math.floor(arr.length / 2)) {
+    return element;
+  }
+  return -1;
+}
+console.log(majorityElement1([4, 4, 2, 4, 3, 4, 4, 3, 2, 4]));
+
+function kadaneAlgorithm(arr) {
+  let sum = 0;
+  let maxi = 0;
+  for (let i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    maxi = Math.max(maxi, sum);
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+  return maxi;
+}
+
+console.log(kadaneAlgorithm([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
+console.log(kadaneAlgorithm([1]));
