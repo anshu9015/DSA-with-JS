d3b55444ce43cc4555c4525414adef47e23fcd0f
Anshu Kumar <anshu.kumar@velocis.co.in>
2025-08-13 18:26:10 +0530

    dutch national flag

--- StriverArray/ques2.js
+++ StriverArray/ques2.js
@@ -69,4 +69,25 @@ function sort012(arr) {
   }
   return arr;
 }
-console.log(sort012([2,0,2,1,1,0]));
+console.log(sort012([2, 0, 2, 1, 1, 0]));
+
+function sort102(arr) {
+  let low = 0;
+  let mid = 0;
+  let high = arr.length - 1;
+  while (mid <= high) {
+    if (arr[mid] === 0) {
+      [arr[low], arr[mid]] = [arr[mid], arr[low]];
+      low++;
+      mid++;
+    } else if (arr[mid] === 1) {
+      mid++;
+    } else {
+      [arr[mid], arr[high]] = [arr[high], arr[mid]];
+      high--;
+    }
+  }
+  return arr;
+}
+
+console.log(sort102([2, 0, 2, 1, 1, 0]));
