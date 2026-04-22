70e2e00915b5ba02b49b8dd57af163d508474d90
anshu9015 <anshu.kmr9015@gmail.com>
2026-04-22 18:23:49 +0530

    custom power function

--- Maths/ques1.js
+++ Maths/ques1.js
@@ -40,7 +40,6 @@ function reverseNumber(n) {
 }
 console.log(reverseNumber(12345));
 
-
 function checkPallindrom(n) {
   let reversedValue = reverseNumber(n);
   if (n === reversedValue) {
@@ -51,111 +50,131 @@ function checkPallindrom(n) {
 
 console.log(checkPallindrom(121));
 
-
-function findGCD(a,b){
-    while(b !== 0){
-        let r = a%b;
-        a = b;
-        b = r;
-    }
-    return a;
+function findGCD(a, b) {
+  while (b !== 0) {
+    let r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
 }
 
-console.log(findGCD(28,20))
+console.log(findGCD(28, 20));
 
-function gcd1(a,b){
-    if(b===0) return a;
-    return gcd1(b,a%b);
+function gcd1(a, b) {
+  if (b === 0) return a;
+  return gcd1(b, a % b);
 }
-console.log(gcd1(28,20))
+console.log(gcd1(28, 20));
 
-
-function gcd3(a,b){
-    let gcd = 1;
-    for(let i = 1;i<=Math.min(a,b);++i){
-        if(a%i===0 && b%i===0){
-            gcd = i;
-        }
+function gcd3(a, b) {
+  let gcd = 1;
+  for (let i = 1; i <= Math.min(a, b); ++i) {
+    if (a % i === 0 && b % i === 0) {
+      gcd = i;
     }
-    return gcd;
+  }
+  return gcd;
 }
-console.log(gcd3(28,20));
+console.log(gcd3(28, 20));
 
-
-function ArmstrongNumber(n){
-    let count = 0;
-    let value = n;
-    let temp = n;
-    let sum = 0;
-    while(n>0){
-        n= Math.floor(n/10);
-        count++;
-    }
-    while(value>0){
-        let digit = value%10;
-        sum += Math.pow(digit,count);
-        value = Math.floor(value/10);
-    }
-    if(sum === temp){
-        return true;
-    }
-    return false;
-}
-
-console.log(ArmstrongNumber(153))
-
-
-function printAllDivisor(n){
-    let result = [];
-    for(let i = 1;i<=n;++i){
-        if(n%i===0){
-            result.push(i);
-        }
-    }
-    return result;
-}
-
-console.log(printAllDivisor(36))
-
-
-function printAllDivisor1(n){
-    let sqrt = Math.sqrt(n);
-    let res = [];
-    for(let i = 1;i<=sqrt;++i){
-        if(n%i===0){
-            res.push(i);
-            if(i !== n/i){
-                res.push(n/i);
-            }
-        }
-    }
-    return res;
-}
-console.log(printAllDivisor1(36))
-
-
-
-function checkPrime(n){
-    let count = 0;
-    for(let i = 1;i<=n;++i){
-        if(n%i===0){
-            count++;
-        }
-    }
-    if(count===2){
-        return true;
-    }
-    return false;
-}
-console.log(checkPrime(23))
-
-function checkPrime(n){
-    if(n<=1) return false;
-    for(let i = 2;i<=Math.sqrt(n);++i){
-        if(n%i===0){
-            return false
-        }
-    }
+function ArmstrongNumber(n) {
+  let count = 0;
+  let value = n;
+  let temp = n;
+  let sum = 0;
+  while (n > 0) {
+    n = Math.floor(n / 10);
+    count++;
+  }
+  while (value > 0) {
+    let digit = value % 10;
+    sum += Math.pow(digit, count);
+    value = Math.floor(value / 10);
+  }
+  if (sum === temp) {
     return true;
+  }
+  return false;
+}
+
+console.log(ArmstrongNumber(153));
+
+function printAllDivisor(n) {
+  let result = [];
+  for (let i = 1; i <= n; ++i) {
+    if (n % i === 0) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+console.log(printAllDivisor(36));
+
+function printAllDivisor1(n) {
+  let sqrt = Math.sqrt(n);
+  let res = [];
+  for (let i = 1; i <= sqrt; ++i) {
+    if (n % i === 0) {
+      res.push(i);
+      if (i !== n / i) {
+        res.push(n / i);
+      }
+    }
+  }
+  return res;
+}
+console.log(printAllDivisor1(36));
+
+function checkPrime(n) {
+  let count = 0;
+  for (let i = 1; i <= n; ++i) {
+    if (n % i === 0) {
+      count++;
+    }
+  }
+  if (count === 2) {
+    return true;
+  }
+  return false;
+}
+console.log(checkPrime(23));
+
+function checkPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); ++i) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 console.log(checkPrime(44));
+
+function power(a, b) {
+  let result = Math.pow(a, b);
+  return result;
+}
+console.log(power(3, 4));
+console.log(power(2, -2));
+
+function customPower(a, b) {
+  let ans = 1;
+  let i = 0;
+  if (b > 0) {
+    for (let i = 0; i < b; ++i) {
+      ans = ans * a;
+    }
+  } else if (b < 0) {
+    for (let i = 0; i < Math.abs(b); ++i) {
+      ans = ans / a;
+    }
+  } else {
+    return 1;
+  }
+  return ans;
+}
+console.log(customPower(2, 4));
+console.log(customPower(2, -2));
+console.log(customPower(2, 0));
