a23da6322df0f136ac08ec02ee6ff596fe46204a
anshu9015 <anshu.kmr9015@gmail.com>
2026-06-14 13:25:31 +0530

    new revision pattern

---
+++ NewRevision/Pattern/pattern1.js
@@ -0,0 +1,71 @@
+function ques1(n) {
+  let str = "";
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < n; ++j) {
+      str += "*" + " ";
+    }
+    str += "\n";
+  }
+  return str;
+}
+console.log(ques1(5));
+
+function ques2(n) {
+  let str = "";
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < i + 1; ++j) {
+      str += "*" + " ";
+    }
+    str += "\n";
+  }
+  return str;
+}
+console.log(ques2(5));
+
+function ques3(n) {
+  let str = "";
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < i + 1; ++j) {
+      str += j + 1 + " ";
+    }
+    str += "\n";
+  }
+  return str;
+}
+console.log(ques3(5));
+
+function ques4(n) {
+  let str = "";
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < i + 1; ++j) {
+      str += i + 1 + " ";
+    }
+    str += "\n";
+  }
+  return str;
+}
+console.log(ques4(5));
+
+function ques5(n) {
+  let str = "";
+  for (let i = n; i > 0; --i) {
+    for (let j = i; j > 0; --j) {
+      str += "*" + " ";
+    }
+    str += "\n";
+  }
+  return str;
+}
+console.log(ques5(5));
+
+function ques6(n) {
+  let str = "";
+  for (let i = n; i > 0; --i) {
+    for (let j = 0; j < i; ++j) {
+      str += j + 1 + " ";
+    }
+    str += "\n";
+  }
+  return str;
+}
+console.log(ques6(5));
